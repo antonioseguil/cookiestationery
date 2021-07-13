e891fefa64cec4d52000b65963c0c000500c7e14
Antonio Seguil <antonioseguil@gmail.com>
2021-07-12 20:22:17 -0500

    Agregado libreria de alertas.

--- src/app/controller/ClientController.js
+++ src/app/controller/ClientController.js
@@ -1,14 +1,18 @@
 const { ipcRenderer } = require("electron");
+const Swal = require("sweetalert2");
 
 const nameController = "clientController";
 const controller = [
   "$scope",
   function ($scope) {
+    /* Conectando con el proceso principal */
     $scope.data = ipcRenderer.sendSync("findAllClient", "");
+    /* Titulo de la vista */
     $scope.title = "Lista de clientes";
+    /* Objeto cliente */
     $scope.cliente = {};
 
-    /* Eventos */
+    /* Eventos para agregar nuevo cliente*/
     $scope.addCliente = function () {
       /* Capturando valor */
       let data = $scope.cliente;
@@ -19,6 +23,23 @@ const controller = [
       $scope.data.push(newCliente);
       /* Limpiando inputs */
       $scope.clean();
+      /* Enviando alerta */
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 4000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener("mouseenter", Swal.stopTimer);
+          toast.addEventListener("mouseleave", Swal.resumeTimer);
+        },
+      });
+
+      Toast.fire({
+        icon: "Cliente Guardado.",
+        title: "¡El cliente fue guardado con exito!",
+      });
     };
 
     /* helpers */
